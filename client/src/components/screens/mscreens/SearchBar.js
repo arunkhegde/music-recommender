71d234ffc4eca5ed99554e5b4fb4875a28b075d5
Arun K Hegde <arunkhegde13@gmail.com>
2021-06-08 16:33:04 +0530

    frontend mainpage

--- client/src/components/screens/mscreens/SearchBar.js
+++ client/src/components/screens/mscreens/SearchBar.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../css/SearchBar.css'
 export const SearchBar = () => {
+    const [sugSongs, setsugSongs] = useState(['song1','song2','song3'])
     return (
         <div className="Smain">
             <input type={Text} className="search"/>
             <div className="suggestions" >
-                <div>sddada</div>
-                <div>sDdkod</div>
+                {sugSongs.map((x)=>{
+                    return <div class="sugsongs">{x}</div>
+                })}
             </div>
         </div>
     )
